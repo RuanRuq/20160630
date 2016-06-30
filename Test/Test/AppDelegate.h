e3ff8c8df5e61f0fea45ab024075e5ad8fcdaed3
zhangsan <zhangsan@163.com>
2016-06-30 16:16:13 +0800

    初始化项目

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Ruq on 16/6/30.
+//  Copyright © 2016年 Warthog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
